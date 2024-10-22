6fda17e7c72d4f93211d701d2d0d2ad3bc24f862
AMarsel2551 <m.adbullinn@gmail.com>
2024-10-22 17:33:10 +0300

    edit

--- app/main.py
+++ app/main.py
@@ -1,3 +1,23 @@
-from scraper import Scraper
+import time
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
 
-pretty_html = Scraper().scrape(link="https://api.ipify.org/").prettify()
+options = webdriver.ChromeOptions()
+options.add_argument('--headless')
+
+path = ChromeDriverManager().install()
+print(f"path: {path}")
+driver = webdriver.Chrome(path, options=options)
+
+
+driver.get('https://api.ipify.org')
+time.sleep(1)
+
+
+main_page = driver.page_source
+print(main_page)
+
+
+time.sleep(2)
+driver.quit()
+
