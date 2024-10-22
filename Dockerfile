1ce9ccf69b75ddbda8d1614c946a8bc81a110a52
AMarsel2551 <m.adbullinn@gmail.com>
2024-10-22 20:18:47 +0300

    edit

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,29 @@
-# Используем официальный образ Python
 FROM python:3.11-slim
 
-# Устанавливаем рабочую директорию в /app
+# Установите необходимые пакеты
+RUN apt-get update && apt-get install -y \
+    wget \
+    unzip \
+    gnupg \
+    curl \
+    libxss1 \
+    libappindicator3-1 \
+    libasound2 \
+    libatk1.0-0 \
+    libcups2 \
+    libx11-xcb1 \
+    libxcomposite1 \
+    libxrandr2 \
+    libxtst6 \
+    libnss3 \
+    libgtk-3-0 \
+    chromium \
+    chromium-driver \
+    && apt-get clean \
+    && rm -rf /var/lib/apt/lists/*
+
 WORKDIR /
 
-# Копируем файлы зависимостей и устанавливаем их
 COPY requirements.txt .
 RUN pip install -r requirements.txt
 
--- app/main.py
+++ app/main.py
@@ -1,23 +1,36 @@
 import time
+import traceback
+
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 
-options = webdriver.ChromeOptions()
-options.add_argument('--headless')
 
-path = ChromeDriverManager().install()
-print(f"path: {path}")
-driver = webdriver.Chrome(path, options=options)
+try:
+    options = webdriver.ChromeOptions()
+    options.add_argument("--headless")  # Запуск без интерфейса
+    options.add_argument("--no-sandbox")  # Обход некоторых проблем с безопасностью
+    options.add_argument("--disable-dev-shm-usage")  # Уменьшение использования памяти
 
-
-driver.get('https://api.ipify.org')
-time.sleep(1)
-
-
-main_page = driver.page_source
-print(main_page)
+    path = ChromeDriverManager().install()
+    print(f"path: {path}")
+    driver = webdriver.Chrome(path, options=options)
 
 
-time.sleep(2)
-driver.quit()
+    driver.get('https://api.ipify.org')
+    time.sleep(1)
+
+
+    main_page = driver.page_source
+    print(main_page)
+
+
+    time.sleep(2)
+    driver.quit()
+except:
+    print(f"traceback: {traceback.format_exc()}")
+
+
+print("END")
+time.sleep(5)
+
 
