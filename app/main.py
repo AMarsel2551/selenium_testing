cd8fc6aa8182230b656d4c48c34a70b8859f8dff
AMarsel2551 <m.adbullinn@gmail.com>
2024-10-23 14:21:04 +0300

    edit

--- app/main.py
+++ app/main.py
@@ -1,8 +1,6 @@
 import time
 import traceback
-
 from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
 
 
 try:
@@ -10,12 +8,7 @@ try:
     options.add_argument("--headless")  # Запуск без интерфейса
     options.add_argument("--no-sandbox")  # Обход некоторых проблем с безопасностью
     options.add_argument("--disable-dev-shm-usage")  # Уменьшение использования памяти
-
-    # path = ChromeDriverManager().install()
-    # print(f"path 1: {path}")
-    path = "/app/chromedriver"
-    print(f"path 2: {path}")
-    driver = webdriver.Chrome(path, options=options)
+    driver = webdriver.Chrome("/app/chromedriver", options=options)
 
 
     driver.get('https://api.ipify.org')
